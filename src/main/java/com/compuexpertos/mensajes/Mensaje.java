5363b80aa309c0571a3c8619fa94ceebe907e2b8
uerodas <efrenrodas1@gmail.com>
2021-03-31 19:38:19 -0500

    primer commit

---
+++ src/main/java/com/compuexpertos/mensajes/Mensaje.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.compuexpertos.mensajes;
+
+/**
+ *
+ * @author efren
+ */
+public class Mensaje {
+    private int id;
+    private String mensaje;
+    private String autor;
+    private String fecha;
+
+    public Mensaje(int id) {
+        this.id = id;
+    }
+
+    public Mensaje(String mensaje, String autor) {
+        this.mensaje = mensaje;
+        this.autor = autor;
+    }
+
+    public Mensaje(int id, String mensaje, String autor) {
+        this.id = id;
+        this.mensaje = mensaje;
+        this.autor = autor;
+    }
+
+    public Mensaje(int id, String mensaje, String autor, String fecha) {
+        this.id = id;
+        this.mensaje = mensaje;
+        this.autor = autor;
+        this.fecha = fecha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    @Override
+    public String toString() {
+        return "Mensaje{" + "id=" + id + ", mensaje=" + mensaje + ", autor=" + autor + ", fecha=" + fecha + '}';
+    }
+    
+    
+}
